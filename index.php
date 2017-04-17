9528e2ac1169f87942082464a1b2e1c05e2f621c
SgtBagus <procw57@gmail.com>
2017-04-17 14:55:44 +0700

    versi 7.8

--- index.php
+++ index.php
@@ -201,17 +201,17 @@ session_start();
       }
       while($data2 = mysqli_fetch_assoc($result2)){ 
                     echo '<a href="system/notifikasi_pengajuan.php?id='.$data2['id_riwayat'].'" style="color:black">';
-    
-    if ($data2['notifikasi'] == "1"){
-                        echo '<div class="card"  style="background-color:#f2f2f2">';
-    }
-    else{
                         echo '<div class="card">';
-
-    }
                             echo '<div class="content">';
                                 echo '<input type="hidden" name="id_pengajuan" value="'.$data2['id_pengajuan'].'">';
-                                echo '<h5><b>'.$data2['jenis_riwayat'].'</b> - <small>'.$data2['kegiatan3'].'</small></h5>';
+                                echo '<h5><b>'.$data2['jenis_riwayat'].'</b> - <small>'.$data2['kegiatan3'].'</small></h5>';                           
+    if ($data2['notifikasi'] == "1"){
+                            echo '<div align="right">';
+                                echo '<span class="new badge">Belum Dibaca</span>';
+                            echo '</div>';
+    }
+    else{
+    }
                             echo '</div>';
                         echo '</div>';
                     echo '</a>';
